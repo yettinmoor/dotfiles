e3da22234617672c0902259531f5ce6da51e3fb5
Nicolas Lindström <nicolas.lindstroem@protonmail.com>
2024-07-30 18:00:02 +0200

    autocommit talos 2024-07-30

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -120,17 +120,18 @@ config.bind(
 
 # adblock
 c.content.blocking.adblock.lists = [
+    "https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt",
+    "https://easylist-downloads.adblockplus.org/easylistdutch.txt",
     "https://easylist.to/easylist/easylist.txt",
     "https://easylist.to/easylist/easyprivacy.txt",
-    "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
     "https://easylist.to/easylist/fanboy-annoyance.txt",
-    "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
-    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt",
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
-    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
-    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt"
+    "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt",
+    "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
+    "https://www.i-dont-care-about-cookies.eu/abp/"
 ]
 c.content.blocking.enabled = True
 c.content.blocking.hosts.lists = [
