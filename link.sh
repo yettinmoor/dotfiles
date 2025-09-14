d1b7d5d67232596725ea9ad30b177442133d5f78
Nicolas Lindström <nicolas.lindstroem@protonmail.com>
2025-09-14 23:41:33 +0200

    autocommit medea 2025-09-14

--- link.sh
+++ link.sh
@@ -2,7 +2,7 @@
 
 cd ~
 
-ln -sf repos/dotfiles/config .config
-ln -sf repos/dotfiles/local  .local
-ln -sf .config/xorg/xinitrc  .xinitrc
-ln -sf .config/profile       .zprofile
+ln -sf repos/dotfiles/config              .config
+ln -sf repos/dotfiles/config/applications ~/.local/share
+ln -sf .config/xorg/xinitrc               .xinitrc
+ln -sf .config/profile                    .zprofile
